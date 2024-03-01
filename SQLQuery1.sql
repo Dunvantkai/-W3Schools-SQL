010c5d4c315d680a861e880d94ee58c4a24e4826
Dunvant_kai <77610897+Dunvantkai@users.noreply.github.com>
2024-03-01 15:09:39 +1300

    Create SQLQuery1.sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Customers (
+	CustomerID INT PRIMARY KEY,
+	CustomerName VARCHAR(255),
+	ContactName VARCHAR(255),
+	Address VARCHAR(255),
+	City VARCHAR(255),
+	PostalCode VARCHAR(10),
+	Country VARCHAR(255)
+);
+
